fb854d62b28cc8842fdd56754e7d48a592f1c49d
monirol4664-glitch <monirol4664@gmail.com>
2026-04-20 12:47:27 +0600

    Create MauiProgram.cs

---
+++ MauiProgram.cs
@@ -0,0 +1,30 @@
+using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+using Microsoft.Maui.Controls.Hosting;
+
+namespace MathToolsApp;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+        return builder.Build();
+    }
+}
+
+public class App : Application
+{
+    public App()
+    {
+        MainPage = new NavigationPage(new MainPage());
+    }
+}
