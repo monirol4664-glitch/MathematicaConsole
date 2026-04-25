c719a3d42695a550aa0f58016827483f4462bf1b
monirol4664-glitch <monirol4664@gmail.com>
2026-04-25 22:12:16 +0600

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -4,45 +4,22 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebSettings;
-import android.webkit.WebChromeClient;
-import android.view.KeyEvent;
 
 public class MainActivity extends Activity {
-    private WebView webView;
-    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        webView = new WebView(this);
-        WebSettings webSettings = webView.getSettings();
+        WebView webView = new WebView(this);
+        WebSettings settings = webView.getSettings();
         
-        // Optimize for Python IDE
-        webSettings.setJavaScriptEnabled(true);
-        webSettings.setDomStorageEnabled(true);
-        webSettings.setDatabaseEnabled(true);
-        webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
-        webSettings.setAllowFileAccess(true);
-        webSettings.setAllowFileAccessFromFileURLs(true);
-        webSettings.setAllowUniversalAccessFromFileURLs(true);
-        webSettings.setLoadWithOverviewMode(true);
-        webSettings.setUseWideViewPort(true);
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setAllowFileAccessFromFileURLs(true);
+        settings.setAllowUniversalAccessFromFileURLs(true);
         
-        // Enable debugging
-        WebView.setWebContentsDebuggingEnabled(true);
-        webView.setWebChromeClient(new WebChromeClient());
-        
-        // Load the Python IDE
         webView.loadUrl("file:///android_asset/index.html");
         setContentView(webView);
-    }
-    
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
-            webView.goBack();
-            return true;
-        }
-        return super.onKeyDown(keyCode, event);
     }
 }
